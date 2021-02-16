a9e454a09aa1602752556d6760b36b65e828ff81
Minakshee Kumari <minakshee.x.kumari@gsk.com>
2021-02-16 16:05:57 +0530

    initial commit

---
+++ IPManagementSrevice/src/main/java/ip/management/service/IPManagementSrevice/IpManagementSreviceApplication.java
@@ -0,0 +1,13 @@
+package ip.management.service.IPManagementSrevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpManagementSreviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpManagementSreviceApplication.class, args);
+	}
+
+}
---
+++ IPManagementSrevice/src/test/java/ip/management/service/IPManagementSrevice/IpManagementSreviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ip.management.service.IPManagementSrevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpManagementSreviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
